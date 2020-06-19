20ef94d56eb5c341ac303dbea98861cfa3e607f9
Dmitry Vasilev <dmitry@t3dev.ru>
2020-06-19 18:19:44 +0300

    Changes directly on server

--- src/tmpl/Resources/Public/JavaScript/custom.js
+++ src/tmpl/Resources/Public/JavaScript/custom.js
@@ -1,13 +1,10 @@
-jQuery(document).ready(function() {
-  // Tabs
-  $('.tab-content .tab-pane:first').addClass('active');
-  // Lightbox
-  $('.popup-gallery a.popup, .typo3-neos-nodetypes-textwithimage a.popup, .typo3-neos-nodetypes-image a.popup, a.popup, .wmg-details-content a.popup').lightbox();
-  //$('.sites .typo3-neos-nodetypes-textwithimage a')();
+$(function() {
+    // Tabs
+    $('.tab-content .tab-pane:first').addClass('active');
 
   if ($("div.content").height() > 1000) {
     $(".content").addClass("content-height-1000");
-  }
+     }
   if ($("div.content").height() > 2500) {
     $(".content").addClass("content-height-2500");
   }
